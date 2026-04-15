93d00707233ff30bf0519579f74c087c78172997
subhani153 <shaik.subhani153@gmail.com>
2026-04-15 21:00:07 +0530

    installation check

---
+++ colors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+CHECK(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 ... $R Failed"
+        exit 1
+    else
+        echo "$2 ... $G Sucess"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Error:: you need sudo permission"
+    exit 1
+fi
+
+dnf list installed mysql
+if [$? -ne 0]
+then 
+    dnf instll mysql -y
+    CHECK $? "Installating mysql"
+else
+    echo "mysql already... $Y installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    CHECK $? "Installing Git"
+else
+    echo "Git already.... $Y installed"
+fi
