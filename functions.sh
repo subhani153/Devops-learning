5f4df5fea1516f4d42fe2e47428d9694249e9d7f
subhani153 <shaik.subhani153@gmail.com>
2026-04-15 20:40:28 +0530

    installation check

---
+++ functions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+CHECK(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 ... Failed"
+        exit 1
+    else
+        echo "$2 ... Suceess"
+    fi
+}
+
+if [ $USERID -e 0 ]
+then 
+    echo "Error:: You need sudo permission"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    CHECK $? "Installing Mysql"
+else
+    echo "mysql is already installed"
+fi
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y
+    CHECK $? "Installing Git"
+else
+    echo "Git already installed"
+fi
